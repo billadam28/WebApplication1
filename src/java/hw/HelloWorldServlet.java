6332cc541b19cd751ec35a21d8f354a92f863214
billadam28 <adamopoulos.vas@gmail.com>
2016-04-10 20:56:32 +0300

    commit to branch
    test

--- src/java/hw/HelloWorldServlet.java
+++ src/java/hw/HelloWorldServlet.java
@@ -76,6 +76,7 @@ public class HelloWorldServlet extends HttpServlet {
             else { out.println(myOutpout);}
             // geia sas 
             // hello world
+            // branch allagh
             
             
         }
